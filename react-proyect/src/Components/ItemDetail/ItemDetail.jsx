f732e02b9b2e527c7cc59fd77d6489f2a54b3d8d
mxrtin-5 <martineluney023@gmail.com>
2023-08-03 14:51:34 -0300

    ajuste imagen detalle

--- react-proyect/src/Components/ItemDetail/ItemDetail.jsx
+++ react-proyect/src/Components/ItemDetail/ItemDetail.jsx
@@ -28,8 +28,8 @@ export const ItemDetail = ({item}) =>{
     return(
         <div className="container my-5">
             <h2>{item.name}</h2>
-            <img src={item.img} alt={item.name} />
-            <p>Precio: ${item.price}</p>
+            <img className='imagen' src={item.img} alt={item.name} />
+            <p className='p-detail'>Precio: ${item.price}</p>
 
 
             {
