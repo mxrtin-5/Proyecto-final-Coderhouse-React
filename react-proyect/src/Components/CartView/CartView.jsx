812601ed617f86e6cecd298bc022ee776fe4659c
mxrtin-5 <martineluney023@gmail.com>
2023-08-23 19:17:47 -0300

    Ajustes responsive

--- react-proyect/src/Components/CartView/CartView.jsx
+++ react-proyect/src/Components/CartView/CartView.jsx
@@ -13,7 +13,7 @@ export const CartView = () => {
         const encodedMessage = encodeURIComponent(productsMessage);
         return `https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`;
     };
-    
+
     const generateProductsMessage = (cart) => {
         const productLines = cart.map((product) => {
             return `${product.name} - Cantidad: ${product.counter}`;
@@ -25,23 +25,23 @@ export const CartView = () => {
         <div className='container-cart-products'>
             <h2>Carrito de Compra</h2>
             <div className='cart-products'>
-            {cart.length === 0 ? (
-            <p>No hay productos en el carrito</p>
-        ) : (
-            cart.map((product) => (
-                <div key={product.id} className='cart-product'>
-                    <img src={product.img} alt={product.name} className='product-image' />
-                    <div className='info-cart-product'>
-                        <div className='titulo-producto-carrito'>{product.name}</div>
-                        <div className='precio-producto-carrito'>${product.price}</div>
-                        <div className='cantidad-producto-carrito'>Cantidad: {product.counter}</div>
-                    </div>
-                    <div className='icon-close' onClick={() => removerDelCarrito(product.id)}>
-                        <FaTrashAlt />
-                    </div>
-                </div>
-            ))
-        )}
+                {cart.length === 0 ? (
+                    <p>No hay productos en el carrito</p>
+                ) : (
+                    cart.map((product) => (
+                        <div key={product.id} className='cart-product'>
+                            <img src={product.img} alt={product.name} className='product-image' />
+                            <div className='info-cart-product'>
+                                <div className='titulo-producto-carrito'>{product.name}</div>
+                                <div className='precio-producto-carrito'>${product.price}</div>
+                                <div className='cantidad-producto-carrito'>{product.counter}</div>
+                            </div>
+                            <div className='icon-close' onClick={() => removerDelCarrito(product.id)}>
+                                <FaTrashAlt />
+                            </div>
+                        </div>
+                    ))
+                )}
                 {cart.length > 0 && (
                     <div className='cart-total'>
                         <button onClick={vaciarCarrito} className='btn-terminar-compra-cart'> <a target='_blank' href={generateWhatsAppLink(cart)}>Terminar Compra</a> </button>
