5fa824c35ba11a80a0467dfbcb2bb8b9efb0d9f0
mxrtin-5 <martineluney023@gmail.com>
2023-08-27 16:39:32 -0300

    Ajuste de estilos

--- react-proyect/src/Components/Presentation/Presentation.jsx
+++ react-proyect/src/Components/Presentation/Presentation.jsx
@@ -8,6 +8,7 @@ export const Presentation = () =>{
         <section className='contenedor-portada'>
             <div id='img-portada' className="contenedor-portada">
                 
+                <img src="/descarga_ml_resize_x4.jpg" className='literalmente-la-imagen' />
                     <div className='contenedor-titulo-presentacion'>
                         <h1 className='titulo-presentacion' >Revolution</h1>
                         <p className='subtitulo-presentacion'>Precision and Performance in Motion:<br/> Soluciones de Rodamientos Innovadores para Impulsar tu Éxito</p>
